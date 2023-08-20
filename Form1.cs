98eeebf049eafe1688293c5670c0d21acc93fa23
Juan De Leon <ds23003@ues.edu.sv>
2023-08-20 18:56:38 +0100

    Proyecto listo para empezar a trabajarlo

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;//Esta biblioteca hace posible el hacer uso de audios
+using NAudio;
+using System.IO;//Biblioteca que permite el manejo, uso de entrada y salida de archivos
+using TagLib;//Biblioteca para trabajar con metadatos de audio
+namespace Tarea_de_investigacion_ReproductorMusical
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
