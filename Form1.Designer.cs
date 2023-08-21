445dbf01e9d6d3f487124c1478e058eb19864a5e
LupitaStrada <lupitastrada1002@gmail.com>
2023-08-20 22:14:03 -0600

    ghhh

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,9 +33,11 @@ namespace Tarea_de_investigacion_ReproductorMusical
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1177, 450);
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1766, 692);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
