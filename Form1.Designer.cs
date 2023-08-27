a2dd26bd190cb24db5890664ace271397a97226c
Juan De Leon <ds23003@ues.edu.sv>
2023-08-27 22:28:11 +0100

    Comentarios agregados y otras funciones...

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,59 +32,96 @@ namespace Tarea_de_investigacion_ReproductorMusical
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panelLateral = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRuta = new System.Windows.Forms.Button();
             this.listBoxMusic = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBarSubirBajarVolumen = new System.Windows.Forms.TrackBar();
             this.btnStop = new System.Windows.Forms.Button();
             this.btnPausa = new System.Windows.Forms.Button();
             this.btnPlay = new System.Windows.Forms.Button();
             this.imglFotos = new System.Windows.Forms.ImageList(this.components);
             this.lblimagenes = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbltituloCancion = new System.Windows.Forms.Label();
             this.panelLateral.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSubirBajarVolumen)).BeginInit();
             this.SuspendLayout();
             // 
             // panelLateral
             // 
             this.panelLateral.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.panelLateral.Controls.Add(this.label1);
+            this.panelLateral.Controls.Add(this.btnRuta);
             this.panelLateral.Controls.Add(this.listBoxMusic);
             this.panelLateral.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLateral.Location = new System.Drawing.Point(0, 0);
             this.panelLateral.Margin = new System.Windows.Forms.Padding(2);
             this.panelLateral.Name = "panelLateral";
-            this.panelLateral.Size = new System.Drawing.Size(167, 450);
+            this.panelLateral.Size = new System.Drawing.Size(175, 450);
             this.panelLateral.TabIndex = 9;
             this.panelLateral.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(3, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 26);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Music List";
+            // 
+            // btnRuta
+            // 
+            this.btnRuta.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnRuta.BackgroundImage")));
+            this.btnRuta.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnRuta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRuta.Location = new System.Drawing.Point(0, 422);
+            this.btnRuta.Name = "btnRuta";
+            this.btnRuta.Size = new System.Drawing.Size(35, 28);
+            this.btnRuta.TabIndex = 13;
+            this.btnRuta.UseVisualStyleBackColor = true;
+            this.btnRuta.Click += new System.EventHandler(this.btnRuta_Click);
             // 
             // listBoxMusic
             // 
             this.listBoxMusic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(101)))), ((int)(((byte)(187)))));
             this.listBoxMusic.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBoxMusic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.listBoxMusic.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxMusic.ForeColor = System.Drawing.Color.White;
             this.listBoxMusic.FormattingEnabled = true;
-            this.listBoxMusic.Location = new System.Drawing.Point(6, 120);
+            this.listBoxMusic.Location = new System.Drawing.Point(8, 67);
             this.listBoxMusic.Margin = new System.Windows.Forms.Padding(2);
             this.listBoxMusic.Name = "listBoxMusic";
-            this.listBoxMusic.Size = new System.Drawing.Size(157, 208);
+            this.listBoxMusic.Size = new System.Drawing.Size(157, 338);
             this.listBoxMusic.TabIndex = 12;
             this.listBoxMusic.SelectedIndexChanged += new System.EventHandler(this.listBoxMusic_SelectedIndexChanged);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(101)))), ((int)(((byte)(187)))));
+            this.panel1.Controls.Add(this.lbltituloCancion);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(167, 0);
+            this.panel1.Location = new System.Drawing.Point(175, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(542, 68);
+            this.panel1.Size = new System.Drawing.Size(534, 68);
             this.panel1.TabIndex = 10;
             // 
-            // trackBar1
+            // trackBarSubirBajarVolumen
             // 
-            this.trackBar1.Location = new System.Drawing.Point(409, 387);
-            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(237, 45);
-            this.trackBar1.TabIndex = 11;
+            this.trackBarSubirBajarVolumen.Location = new System.Drawing.Point(442, 391);
+            this.trackBarSubirBajarVolumen.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBarSubirBajarVolumen.Name = "trackBarSubirBajarVolumen";
+            this.trackBarSubirBajarVolumen.Size = new System.Drawing.Size(223, 45);
+            this.trackBarSubirBajarVolumen.TabIndex = 11;
+            this.trackBarSubirBajarVolumen.Value = 5;
+            this.trackBarSubirBajarVolumen.Scroll += new System.EventHandler(this.trackBarSubirBajarVolumen_Scroll);
             // 
             // btnStop
             // 
@@ -96,6 +133,7 @@ namespace Tarea_de_investigacion_ReproductorMusical
             this.btnStop.Size = new System.Drawing.Size(37, 31);
             this.btnStop.TabIndex = 6;
             this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
             // 
             // btnPausa
             // 
@@ -107,6 +145,7 @@ namespace Tarea_de_investigacion_ReproductorMusical
             this.btnPausa.Size = new System.Drawing.Size(39, 31);
             this.btnPausa.TabIndex = 5;
             this.btnPausa.UseVisualStyleBackColor = true;
+            this.btnPausa.Click += new System.EventHandler(this.btnPausa_Click);
             // 
             // btnPlay
             // 
@@ -142,14 +181,48 @@ namespace Tarea_de_investigacion_ReproductorMusical
             this.lblimagenes.TabIndex = 12;
             this.lblimagenes.Text = ".";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(453, 422);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "-";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(649, 422);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "+";
+            // 
+            // lbltituloCancion
+            // 
+            this.lbltituloCancion.AutoSize = true;
+            this.lbltituloCancion.Font = new System.Drawing.Font("Microsoft YaHei", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltituloCancion.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbltituloCancion.Location = new System.Drawing.Point(105, 33);
+            this.lbltituloCancion.Name = "lbltituloCancion";
+            this.lbltituloCancion.Size = new System.Drawing.Size(139, 22);
+            this.lbltituloCancion.TabIndex = 16;
+            this.lbltituloCancion.Text = "Reproduciendo:";
+            this.lbltituloCancion.Click += new System.EventHandler(this.label5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(709, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lblimagenes);
-            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.trackBarSubirBajarVolumen);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panelLateral);
             this.Controls.Add(this.btnStop);
@@ -159,7 +232,10 @@ namespace Tarea_de_investigacion_ReproductorMusical
             this.Text = "Reproductor de Musica";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panelLateral.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panelLateral.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarSubirBajarVolumen)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -171,10 +247,15 @@ namespace Tarea_de_investigacion_ReproductorMusical
         private System.Windows.Forms.Button btnStop;
         private System.Windows.Forms.Panel panelLateral;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBarSubirBajarVolumen;
         private System.Windows.Forms.ListBox listBoxMusic;
         private System.Windows.Forms.ImageList imglFotos;
         private System.Windows.Forms.Label lblimagenes;
+        private System.Windows.Forms.Button btnRuta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbltituloCancion;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,36 +1,42 @@
-﻿using NAudio.Wave;//Esta biblioteca hace posible el hacer uso de audios
-using System;
-using System.IO;//Biblioteca que permite el manejo, uso de entrada y salida de archivos
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using NAudio.Wave;//Esta biblioteca hace posible el hacer uso de audios
+using NAudio;
+using System.IO;//Biblioteca que permite el manejo, uso de entrada y salida de archivos
+
 namespace Tarea_de_investigacion_ReproductorMusical
 {
     public partial class Form1 : Form
     {
         
-        string rutaMusical = @"Musica\"; //Ruta del directorio que contiene la música
-        private WaveOutEvent waveOut;
-
+        private IWavePlayer cancion;//Variable de reproductor de audio
+        private AudioFileReader audioFile;//Variable asignada para el archivo de audio
         private int indice;// Variable de control de las portadas del album
+        private string rutaMusica;
+
         public Form1()
         {
             InitializeComponent();
             indice = 0;// Inicializar desde 0
+            cancion = new WaveOutEvent();//Inicia evento de reproductor de audio
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string[] canciones = Directory.GetFiles(rutaMusical, "*.mp3"); // Arreglo que contendrá la lista de nombres
-            //La siguiente estructura enlista los elementos del arreglo canciones en el listBoxMusic
-            foreach (string cancion in canciones)
-            {
-                string nombreCancion = Path.GetFileName(cancion);
-                listBoxMusic.Items.Add(nombreCancion);
-            }
-            waveOut = new WaveOutEvent();
+           
         }
+        private string tituloCancionActual = "";
+
         private void label1_Click(object sender, EventArgs e)
         {
-            //segundo comentario en la aplicacion 
+            
         }
 
         private void panel1_Paint(object sender, PaintEventArgs e)
@@ -39,27 +45,43 @@ namespace Tarea_de_investigacion_ReproductorMusical
         }
         private void btnPlay_Click(object sender, EventArgs e)
         {
-            indice++;
-            if (indice >= 3) //Ciclo de cambio de imagenes para cada cancion.
-                indice = 0;
-            lblimagenes.ImageIndex = indice; // Repertorio de imagenes almacenadas en el ciclo de imagenes. 
-
-            Reproducir();
-
-            void Reproducir()
+            if (listBoxMusic.SelectedIndex != -1 && !string.IsNullOrEmpty(rutaMusica))
             {
-                string nombreCancion = listBoxMusic.SelectedItem.ToString();
-                string rutaCompleta = Path.Combine(rutaMusical, nombreCancion);
-                if (waveOut != null)
+                //Se comprueba si se selecciono una cacion y se hay una ruta existente de las canciones
+                string selectedSong = Path.Combine(rutaMusica, listBoxMusic.SelectedItem + ".mp3");
+                if (File.Exists(selectedSong))
                 {
-                    waveOut.Stop(); // Detener la reproducción actual, si la hay
-                    AudioFileReader cancion = new AudioFileReader(rutaCompleta); //Variable que almacena el archivo de audio
-                    waveOut.Init(cancion);//Inicializa la instancia de reproducción.
-                    waveOut.Play();//Reproduce el audio
+                    audioFile = new AudioFileReader(selectedSong);
+
+                    cancion.Stop();
+                    cancion.Dispose();
+                    cancion = new WaveOutEvent();
+
+                    cancion.Init(audioFile);
+                    cancion.Play();
+
+                    tituloCancionActual = listBoxMusic.SelectedItem.ToString();
+                    lbltituloCancion.Text = "Reproduciendo: " + tituloCancionActual;
+
+                    indice++;
+                    if (indice >= 3)
+                        indice = 0;
+                    lblimagenes.ImageIndex = indice;//Se actualiza el indice de la portada
+                }
+                else
+                {
+                    MessageBox.Show("El archivo de música no se encuentra en la ruta especificada.");
                 }
             }
+            else
+            {
+                MessageBox.Show("Por favor selecciona una canción y especifica la ruta de canciones.");
+            }
+
 
         }
+
+
 
         private void listBoxMusic_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -68,9 +90,73 @@ namespace Tarea_de_investigacion_ReproductorMusical
         private void pictureBox2_Click(object sender, EventArgs e)
         {
         }
-      
 
-    }
+        private void btnPausa_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            cancion.Stop();
+        }
+
+
+        
+            private void btnRuta_Click(object sender, EventArgs e)
+            {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    rutaMusica = folderDialog.SelectedPath;
+                    ActualizarListaCanciones();
+                }
+            }
+        
 }
-  
+
+private void ActualizarListaCanciones()
+        {
+            if (!string.IsNullOrEmpty(rutaMusica))
+            {
+                string[] archivosMusica = Directory.GetFiles(rutaMusica, "*.mp3");
+                listBoxMusic.Items.Clear();
+                foreach (string archivo in archivosMusica)
+                {
+                    string nombreArchivo = Path.GetFileNameWithoutExtension(archivo);
+                    listBoxMusic.Items.Add(nombreArchivo);
+                }
+            }
+        }
+
+private void trackBarSubirBajarVolumen_Scroll(object sender, EventArgs e)
+        {
+            if (cancion != null)
+            {
+                float nuevoVolumen = (float)trackBarSubirBajarVolumen.Value / trackBarSubirBajarVolumen.Maximum;
+                cancion.Volume = nuevoVolumen;
+            }
+        }
+private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+/*Grupo 7
+ Integrantes: 
+
+
+-Juan Cristobal De Leon Sanchez    DS23003
+
+-Jose Leuis Hernandez Flores    HF23001
+
+-Guadalupe Del Carmen Rivas Estrada      RE23003
+
+-Ana Concepcion Romero Vale      RV19034
+
+ 
+ */
 
